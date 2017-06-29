af98ca0f5ea2a6a829d467a6036e5c2593882def
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-29 02:04:22 -0400

    Comenzamos a trabajar en la primera sección.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+	//Creación del nav
 	$("body").append("<nav><div></div></nav>");
 	$("nav").addClass("nav-wrapper amber");
 	$("div").append("<h5>To Do App</h5>");
-	
-
+	//Creamos la sección
+	var firstSection = document.createElement("section");
+	$("nav").after(firstSection);
+	$(firstSection).addClass("grey lighten-2");
 });
