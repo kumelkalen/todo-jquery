4972c96ec95f45408afa682ae4d11643f9eb4217
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-29 03:15:25 -0400

    Creamos los contenedores de nuestra card, que será la primera parte visible de nuestra sección.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
 	var firstSection = document.createElement("section");
 	$("nav").after(firstSection);
 	$(firstSection).addClass("grey lighten-2");
+	//Row
+	var firstRow = document.createElement("div");
+	$(firstRow).addClass("row");
+	//Columna
+	var firstCol = document.createElement("div");
+	$(firstCol).addClass("col s10 m5");
+	//columna dentro del row
+	$(firstRow).append(firstCol);
+	//row dentro de la columna
+	$(firstSection).append(firstRow);
+	
 });
